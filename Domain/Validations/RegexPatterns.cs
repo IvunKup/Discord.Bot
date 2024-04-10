069e7f1f71e59a3c25eb1d0dc21d92fcc115364c
IvunKup <ivkupel@gmail.com>
2024-04-10 17:13:28 +0300

    refactor: added descriptions in RegexPatterns.cs; replaced public with static.

--- Domain/Validations/RegexPatterns.cs
+++ Domain/Validations/RegexPatterns.cs
@@ -1,10 +1,21 @@
 ﻿using System.Text.RegularExpressions;
 
 namespace Domain.Validations;
-
-public class RegexPatterns
+/// <summary>
+/// Регулярки для валидации.
+/// </summary>
+public static class RegexPatterns
 {
-    public readonly Regex PhoneRegex = new Regex(@"^\+373[0-9]{8}$");
-    public readonly Regex NickRegex = new Regex(@"^[a-zA-Z0-9]{2,20}$");
-    public readonly Regex NameRegex = new Regex(@"^[a-zA-Zа-яА-Я]$");
+    /// <summary>
+    /// Телефон
+    /// </summary>
+    public static readonly Regex PhoneRegex = new Regex(@"^\+373[0-9]{8}$");
+    /// <summary>
+    /// Псевданим
+    /// </summary>
+    public static readonly Regex NickRegex = new Regex(@"^[a-zA-Z0-9]{2,20}$");
+    /// <summary>
+    /// ФИО
+    /// </summary>
+    public static readonly Regex NameRegex = new Regex(@"^[a-zA-Zа-яА-Я]$");
 }
