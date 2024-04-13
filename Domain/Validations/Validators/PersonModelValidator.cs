c7f4f173920e234e2e263a54676330f2e9c1061f
IvunKup <ivkupel@gmail.com>
2024-04-13 21:28:03 +0300

    Feat: added: Validator for phone number and for discord nick.

---
+++ Domain/Validations/Validators/PersonModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Domain.Validations.Validators;
+
+public class PersonModelValidator : AbstractValidator<Person>
+{
+    public PersonModelValidator()
+    {
+        RuleFor(p => p.PhoneNumber)
+            .NotNull()
+            .NotEmpty()
+            .Matches(RegexPatterns.PhoneRegex)
+            .WithMessage("Неправильный формат телефона");
+
+        RuleFor(n => n.DiscordNick)
+            .Matches(RegexPatterns.NickRegex)
+            .WithMessage("Неправильный формат псевдонима");
+    }
+}
