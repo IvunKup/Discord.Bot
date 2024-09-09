57a272ecaabfdb17098d4b897e09dfa9d22d79a2
IvanKup <ivkupel@gmail.com>
2024-09-10 00:16:31 +0300

    feat: added: person mapping .

---
+++ Application/Mapping/Person/PersonMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace;
+
+public class PersonMappingProfile
+{
+    public MappingProfile()
+    {
+        CreateMap<FullNameDto, FullName>()
+            .ConstructUsing(dto => new FullName(dto.FirstName, dto.LastName, dto.FatherName));
+        
+        CreateMap<FullName, FullNameDto>();
+    }
+}
