67c86eafa79e8fc95d907989363001bc631c15aa
IvanKup <ivkupel@gmail.com>
2024-09-15 00:45:52 +0300

    feat: added: framework of the FullName Mapping.

--- Application/Mapping/Person/PersonMappingProfile.cs
+++ Application/Mapping/Person/PersonMappingProfile.cs
@@ -5,8 +5,31 @@ public class PersonMappingProfile
     public MappingProfile()
     {
         CreateMap<FullNameDto, FullName>()
-            .ConstructUsing(dto => new FullName(dto.FirstName, dto.LastName, dto.FatherName));
         
-        CreateMap<FullName, FullNameDto>();
+        CreateMap<FullName, FullNameDto>()
+            
+        CreateMap<CreatePersonRequest, Person>()
+
+        CreateMap<Person, CreatePersonRequest>()
+            
+        CreateMap<CreatePersonResponse, Person>()
+
+        CreateMap<Person, CreatePersonResponse>()
+
+		CreateMap<PersonUpdateRequest, Person>()
+
+        CreateMap<Person, CreatePersonResponse>()
+
+		CreateMap<PersonUpdateResponse, Person>()
+
+        CreateMap<Person, CreatePersonResponse>()
+        
+        CreateMap<PersonGetAllResponse, Person>()
+        
+        CreateMap<Person, PersonGetAllResponse>()
+        
+        CreateMap<PersonGetBuildResponse, Person>()
+                
+        CreateMap<Person, PersonGetBuildResponse>();
     }
 }
